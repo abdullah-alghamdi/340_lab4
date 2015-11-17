a087f382a580e598807d7e64350818b605a16e13
Abdullah Alghamdi <alghamdi.abdullah.h@gmail.com>
2015-11-17 09:16:10 -0700

    edit

--- lab4/invKin2D.m
+++ lab4/invKin2D.m
@@ -1,3 +1,12 @@
-function theta=invKin2D(l,theta0,pos,n,mode) 
+function theta=invKin2D(l,theta0,pos) 
+
+theta1 = atan(pos(1)./pos(2))
+theta = [theta0,theta1]';
+
+[pos1,J]=evalRobot2D(l,theta)
+
+err = pos1(:) - pos(:)
+
+end 
     
 
