0c885650418451128090b066edc1dd2905aec07d
Abdullah Alghamdi <alghamdi.abdullah.h@gmail.com>
2015-11-17 23:39:04 -0700

    Broyden done

--- lab4/main.m
+++ lab4/main.m
@@ -1,9 +1,9 @@
-theta1=[pi/2;pi/2]';
+theta1=[pi/3;pi/3]';
 l=[1;1]';
 theta0=[pi/3;pi/3]';
 pos=[0.0;1.7320]';
-%[pos,J]=evalRobot2D(l,theta0)
+%[pos3,J]=evalRobot2D(l,theta0)
 
-[theta]=invKin2D(l,theta1,pos,20, 1)
+[theta]=invKin2D(l,theta1,pos,30, 0)
 
 
